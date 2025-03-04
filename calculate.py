4ef2c2e2ee4abcb800f6157dab08ef1c323c20df
chen jiyan <3036789610@qq.com>
2025-03-04 09:45:50 +0800

    modified:   calculate.py 	modified:   data.csv 	modified:   "\345\215\225\346\221\206\346\265\213\351\207\217\351\207\215\345\212\233\345\212\240\351\200\237\345\272\246.md" 	modified:   "\345\215\225\346\221\206\346\265\213\351\207\217\351\207\215\345\212\233\345\212\240\351\200\237\345\272\246.pdf"

--- calculate.py
+++ calculate.py
@@ -2,4 +2,10 @@ import pandas as pd
 import numpy as np
 import os
 
-df = pd.read_csv('data.csv')
+df = pd.read_csv('data.csv',index_col=0,header=0)
+
+df1=df.mean(axis=1)
+df2=df.std(axis=1)
+df['std'] = df2
+df['mean'] = df1
+print(df1)
