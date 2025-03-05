87fa889e671a3f601a9ab39db1cc9e4092463239
chen jiyan <3036789610@qq.com>
2025-03-05 16:07:35 +0800

    modified:   "\346\227\266\351\227\264.py" 	modified:   "\346\227\266\351\227\264\346\265\213\351\207\217\344\270\255\351\232\217\346\234\272\350\257\257\345\267\256\347\232\204\345\210\206\345\270\203\350\247\204\345\276\213.md"

--- 时间.py
+++ 时间.py
@@ -20,15 +20,15 @@ for i in range(1,n+1):
     mn=min(a[i],mn)
     mx=max(a[i],mx)
     a_bar+=a[i]/n
-print('平均值:',a_bar)
-print('最大值:',mx)
-print('最小值:',mn)
+# print('平均值:',a_bar)
+# print('最大值:',mx)
+# print('最小值:',mn)
 
 sigma=sum([(a[i]-a_bar)**2 for i in range(1,n+1)])/(n-1)
 sigma=sigma**0.5
-print('方差:',sum([(a[i]-a_bar)**2 for i in range(1,n+1)])/(n-1))
-print('标准差:',sum([(a[i]-a_bar)**2 for i in range(1,n+1)])/(n-1)**0.5)
-print('极差:',mx-mn)
+# print('方差:',sum([(a[i]-a_bar)**2 for i in range(1,n+1)])/(n-1))
+# print('标准差:',sum([(a[i]-a_bar)**2 for i in range(1,n+1)])/(n-1)**0.5)
+# print('极差:',mx-mn)
 # pf=pd.DataFrame(a[1:n+1])
 b=[0]*14
 b[0]=2.76+0.03
@@ -41,8 +41,9 @@ cnt=[0]*14
 p=[0]*14
 sigm=[0]*4
 for i in range(1,n+1):
-    for j in range(1,3):
-        
+    for j in range(1,4):
+        if(abs(a[i]-a_bar)<=j*sigma):
+            sigm[j]+=1
     for j in range(12):
         if b[j]-0.03<a[i]<=b[j]+0.03:
             cnt[j]+=1
@@ -52,15 +53,16 @@ for i in range(12):
     cnt[i]=cnt[i]/(n*0.06)#下一行输出markdown表格格式
     p[i]=P(b[i],a_bar,sigma)
    # print("|$(",format(b[i]-0.03,'.2f'),",",format(b[i]+0.03,'.2f'),']$|$',cnt[i],'$|$',format(cnt[i]/n,'.2f'),'$|$',format(cnt[i]/(n*0.06),'.2f'),'$|$',format(P(b[i],a_bar,sigma),'.2f'),'$|')
-
-plt.bar(b[0:12],cnt[0:12],width=0.05)
-plt.plot(b[0:12],p[0:12],color='red',label='正态分布曲线',marker='*')
-plt.xlabel('时间/s')
-plt.ylabel('概率密度/s^-1')
-plt.title('概率密度直方图与正态分布曲线的对比')
-plt.legend()
-plt.savefig('时间.png',dpi=300)
-plt.show()
+for i in range(1,4):
+    print(i,sigm[i],sigm[i]/n)
+# plt.bar(b[0:12],cnt[0:12],width=0.05)
+# plt.plot(b[0:12],p[0:12],color='red',label='正态分布曲线',marker='*')
+# plt.xlabel('时间/s')
+# plt.ylabel('概率密度/s^-1')
+# plt.title('概率密度直方图与正态分布曲线的对比')
+# plt.legend()
+# plt.savefig('时间.png',dpi=300)
+# plt.show()
 
 #pf.to_csv('时间.csv')
 
